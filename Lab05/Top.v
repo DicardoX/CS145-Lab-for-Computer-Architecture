ff4a85829fa421a265f213fb2d939fc824560ff2
Dicardo Xue <49444151+DicardoX@users.noreply.github.com>
2020-06-04 21:19:41 +0800

    Update Top.v

--- Lab05/Top.v
+++ Lab05/Top.v
@@ -69,9 +69,135 @@ module Top(
     wire ZERO;
     /** ALU result */
     wire [31:0] ALURES;
+    //wire [31:0] READMEM;
     
-    wire [31:0] READMEM;
-    
-    
+    /**  Program counter */
+    PC programCounter(
+        .CLK(CLK),
+        .Reset(Reset),
+        .INPUT(PC_IN),
+        .OUTOUT(PC_OUT)
+    );
+    /** Instruction memory */
+    instMemory instMem(
+        .readAddress(PC_OUT),
+        .Instruction(INST)
+    );
+    /** Control signal */
+    Ctr control(
+        .opCode(INST[31:26]),
+        .regDst(REG_DST),
+        .jump(JUMP),
+        .branch(BRANCH),
+        .memRead(MEM_READ),
+        .memToReg(MEM_TO_REG),
+        .aluOp(ALU_OP),
+        .memWrite(MEM_WRITE),
+        .aluSrc(ALU_SRC),
+        .regWrite(REG_WRITE)
+    );
+    /** Register */
+    Reg register(
+        .CLK(CLK),
+        .Reset(Reset),
+        .readReg1(INST[25:21]),
+        .readReg2(INST[20:16]),
+        .writeReg(MUXRES1),
+        .writeData(MUXRES4),
+        .regWrite(REG_WRITE),
+        .readData1(READDATA1),
+        .readData2(READDATA2)
+    );
+    /** Signext */
+    signext signExt(
+        .inst(INST[15:0]),
+        .data(SIGNEXTENDED)
+    );
+    /** ALU Control */
+    ALUCtr aluCtr(
+        .aluCtrOut(ALUCTR),
+        .ALUOp(ALU_OP),
+        .Funct(INST[5:0])
+    );    
+    /** ALU */
+    ALU alu(
+        .in1(READDATA1),
+        .in2(READDATA2),
+        .aluCtr(ALUCTR),
+        .zero(ZERO),
+        .aluRes(ALURES)
+    );
+    /** Data memory */
+    dataMemory dataMem(
+        .CLK(CLK),
+        .address(ALURES),
+        .writeData(READDATA2),
+        .memWrite(MEM_WRITE),
+        .memRead(MEM_READ),
+        .readData(READDATA)
+    ); 
+    /** Adder 1 */
+    Adder adder1(
+        .in1(PC_OUT),
+        .in2(32'b00000000000000000000000000000001),
+        .out(ADDRES1)
+    );
+    /** Adder 2 */
+    Adder adder2(
+        .in1(ADDRES1),
+        .in2(EXTENDSHIFTED),
+        .out(ADDRES2)
+    );
+    /** Left shift 1 */
+    leftShift_2 shift1(
+        .in(INST),
+        .out(INSTSHIFTED)
+    );
+    /** Left shift 2 */
+    leftShift_2 shift2(
+        .in(SIGNEXTENDED),
+        .out(EXTENDSHIFTED)
+    );
+    /** Mux 1 */
+    Mux_5 mux1(
+        .SEL(REG_DST),
+        .in1(INST[20:16]),
+        .in2(INST[15:11]),
+        .out(MUXRES1)
+    );
+    /** Mux 2 */
+    Mux_32 mux2(
+        .SEL(ALU_SRC),
+        .in1(READDATA2),
+        .in2(SIGNEXTENDED),
+        .out(MUXRES2)
+    );
+    /** Mux 3 */
+    Mux_32 mux3(
+        .SEL(AND_GATE_RES),
+        .in1(ADDRES1),
+        .in2(ADDRES2),
+        .out(MUXRES3)
+    );
+    /** Mux 4 */
+    Mux_32 mux4(
+        .SEL(JUMP),
+        .in1(MUXRES3),
+        .in2({ADDRES1[31:28], 2'b00, INSTSHIFTED[25:0]}),
+        .out(PC_IN)
+    );
+    /** Mux 5 */
+    Mux_32 mux5(
+        .SEL(MEM_TO_REG),
+        .in1(ALURES),
+        .in2(READDATA),
+        .out(MUXRES4)
+    );
+    /** And gate */
+    andGate gate(
+        .in1(BRANCH),
+        .in2(ZERO),
+        .out(AND_GATE_RES)
+    );
     
 endmodule
