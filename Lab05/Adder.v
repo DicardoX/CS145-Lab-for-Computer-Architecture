15aead7483a2535375d71d3c4ab7b87c1604d7c3
Dicardo Xue <49444151+DicardoX@users.noreply.github.com>
2020-06-04 17:09:08 +0800

    Create Adder.v

---
+++ Lab05/Adder.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/06/04 17:06:07
+// Design Name: 
+// Module Name: Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Adder(in1, in2, out)
+    input wire [31:0] in1,
+    input wire [31:0] in2,
+    output wire [31:0] out
+    assign out = in1 + in2;
+   
+endmodule
